9ec460009835d7933857dbcac31540d18858abc3
ichik <ichik@yandex.ru>
2011-01-10 00:42:54 +0300

    Vashj-ir and Deepholm

--- SimplerMount.lua
+++ SimplerMount.lua
@@ -21,11 +21,21 @@ SlashCmdList["USEMOUNT"] = function(msg)
 		nofly = string.trim(nofly, " ")
 		if UnitInVehicle("player") and CanExitVehicle() then 
 			VehicleExit() 
-		else
-			if IsMounted() then
-				Dismount()
-			elseif not InCombatLockdown() then
-				if SecureCmdOptionParse("[flyable] t; f") == "t" and ((IsSpellKnown(90267) and (GetCurrentMapContinent()==1 or GetCurrentMapContinent()==2)) or (GetCurrentMapContinent()==4 and IsSpellKnown(54197)) or (GetCurrentMapContinent()==3 and (IsSpellKnown(34090) or IsSpellKnown(90265) or IsSpellKnown(34091))))then
+		elseif IsMounted() then
+			Dismount()
+		elseif not InCombatLockdown() then
+			for i = 1, 40 do
+				local n, _, _, _, _, _, _, _, _, _, sid = UnitBuff("player",i)
+				if (sid == 73701 or sid == 76377) and IsSwimming() then
+					for i=1, GetNumCompanions("MOUNT"), 1 do
+						local _, info, id = GetCompanionInfo("MOUNT", i)
+						if id == 64731 then
+							CallCompanion("MOUNT", i)
+						elseif id == 75207 then
+							CallCompanion("MOUNT", i)
+						end
+					end
+				elseif SecureCmdOptionParse("[flyable] t; f") == "t" and ((IsSpellKnown(90267) and (GetCurrentMapContinent()==1 or GetCurrentMapContinent()==2 or GetCurrentMapContinent()==5)) or (GetCurrentMapContinent()==4 and IsSpellKnown(54197)) or (GetCurrentMapContinent()==3 and (IsSpellKnown(34090) or IsSpellKnown(90265) or IsSpellKnown(34091))))then
 					CastSpellByName(fly)
 				elseif GetZoneText() == WINTERGRASP and GetWintergraspWaitTime() and IsSpellKnown(54197) then
 					CastSpellByName(fly)
